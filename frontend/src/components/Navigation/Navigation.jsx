f4b3e6fc9b049373561482e22ebe882828228368
shakgill786 <sgillani1@gmail.com>
2024-12-31 11:30:35 -0500

    1231stylingupdates

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -2,7 +2,6 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-import { FaUserCircle } from 'react-icons/fa'; // Import Font Awesome Icon
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
